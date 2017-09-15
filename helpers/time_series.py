4445d2c6b73872ba49b1afff9bd5ef0ca1d37671
= <michael.beale@gmail.com>
2017-09-14 22:53:13 -0700

    initial commit

---
+++ helpers/time_series.py
@@ -0,0 +1,295 @@
+import datetime
+import time
+import itertools
+import warnings
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
+import requests
+import librato
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import numpy as np
+
+import seaborn as sns
+
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+def convert_timestamp_to_pd_date(df):
+    df['date'] = [datetime.datetime.fromtimestamp(x) for x in df.index]
+    df.reset_index(inplace=True)
+    df.set_index('date', inplace=True)
+    del df['time']
+    return df
+
+def test_stationarity(timeseries,
+                      maxlag=60, regression=None, autolag=None,
+                      window=None, plot=False, verbose=False):
+    '''
+    Check unit root stationarity of time series.
+
+    Null hypothesis: the series is non-stationary.
+    If p >= alpha, the series is non-stationary.
+    If p < alpha, reject the null hypothesis (has unit root stationarity).
+
+    Original source: http://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
+
+    Function: http://statsmodels.sourceforge.net/devel/generated/statsmodels.tsa.stattools.adfuller.html
+
+    window argument is only required for plotting rolling functions. Default=4.
+    '''
+
+    # set defaults (from function page)
+    if regression is None:
+        regression = 'c'
+
+    if verbose:
+        print('Running Augmented Dickey-Fuller test with paramters:')
+        print('maxlag: {}'.format(maxlag))
+        print('regression: {}'.format(regression))
+        print('autolag: {}'.format(autolag))
+
+    if plot:
+        if window is None:
+            window = 4
+        #Determing rolling statistics
+        rolmean = timeseries.rolling(window=window, center=False).mean()
+        rolstd = timeseries.rolling(window=window, center=False).std()
+
+        #Plot rolling statistics:
+        orig = plt.plot(timeseries, color='blue', label='Original')
+        mean = plt.plot(rolmean, color='red', label='Rolling Mean ({})'.format(window))
+        std = plt.plot(rolstd, color='black', label='Rolling Std ({})'.format(window))
+        plt.legend(loc='best')
+        plt.title('Rolling Mean & Standard Deviation')
+        plt.show(block=False)
+
+    #Perform Augmented Dickey-Fuller test:
+    dftest = smt.adfuller(timeseries, maxlag=maxlag, regression=regression, autolag=autolag)
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic',
+                                             'p-value',
+                                             '#Lags Used',
+                                             'Number of Observations Used',
+                                            ])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    if verbose:
+        print('Results of Augmented Dickey-Fuller Test:')
+        print(dfoutput)
+    return dfoutput
+
+def tsplot(y, lags=60, title='', figsize=(14, 8)):
+    '''Examine the patterns of ACF and PACF, along with the time series plot and histogram.
+    
+    Original source: https://tomaugspurger.github.io/modern-7-timeseries.html
+    '''
+    fig = plt.figure(figsize=figsize)
+    layout = (2, 2)
+    ts_ax   = plt.subplot2grid(layout, (0, 0))
+    hist_ax = plt.subplot2grid(layout, (0, 1))
+    acf_ax  = plt.subplot2grid(layout, (1, 0))
+    pacf_ax = plt.subplot2grid(layout, (1, 1))
+    
+    y.plot(ax=ts_ax)
+    ts_ax.set_title(title)
+    y.plot(ax=hist_ax, kind='hist', bins=25)
+    hist_ax.set_title('Histogram')
+    smt.graphics.plot_acf(y, lags=lags, ax=acf_ax)
+    smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
+    [ax.set_xlim(0) for ax in [acf_ax, pacf_ax]]
+    sns.despine()
+    fig.tight_layout()
+    return ts_ax, acf_ax, pacf_ax
+
+def model_resid_stats(model_results,
+                      het_method='breakvar',
+                      norm_method='jarquebera',
+                      sercor_method='ljungbox',
+                      verbose=True,
+                      ):
+    '''More information about the statistics under the ARIMA parameters table, tests of standardized residuals:
+    
+    Test of heteroskedasticity
+    http://www.statsmodels.org/dev/generated/statsmodels.tsa.statespace.sarimax.SARIMAXResults.test_heteroskedasticity.html#statsmodels.tsa.statespace.sarimax.SARIMAXResults.test_heteroskedasticity
+
+    Test of normality (Default: Jarque-Bera)
+    http://www.statsmodels.org/dev/generated/statsmodels.tsa.statespace.sarimax.SARIMAXResults.test_normality.html#statsmodels.tsa.statespace.sarimax.SARIMAXResults.test_normality
+
+    Test of serial correlation (Default: Ljung-Box)
+    http://www.statsmodels.org/dev/generated/statsmodels.tsa.statespace.sarimax.SARIMAXResults.test_serial_correlation.html
+    '''
+    # Re-run the ARIMA model statistical tests, and more. To be used when selecting viable models.
+    (het_stat, het_p) = model_results.test_heteroskedasticity(het_method)[0]
+    norm_stat, norm_p, skew, kurtosis = model_results.test_normality(norm_method)[0]
+    sercor_stat, sercor_p = model_results.test_serial_correlation(method=sercor_method)[0]
+    sercor_stat = sercor_stat[-1] # last number for the largest lag
+    sercor_p = sercor_p[-1] # last number for the largest lag
+
+    # Run Durbin-Watson test on the standardized residuals.
+    # The statistic is approximately equal to 2*(1-r), where r is the sample autocorrelation of the residuals.
+    # Thus, for r == 0, indicating no serial correlation, the test statistic equals 2.
+    # This statistic will always be between 0 and 4. The closer to 0 the statistic,
+    # the more evidence for positive serial correlation. The closer to 4,
+    # the more evidence for negative serial correlation.
+    # Essentially, below 1 or above 3 is bad.
+    dw_stat = sm.stats.stattools.durbin_watson(model_results.filter_results.standardized_forecasts_error[0, model_results.loglikelihood_burn:])
+
+    # check whether roots are outside the unit circle (we want them to be);
+    # will be True when AR is not used (i.e., AR order = 0)
+    arroots_outside_unit_circle = np.all(np.abs(model_results.arroots) > 1)
+    # will be True when MA is not used (i.e., MA order = 0)
+    maroots_outside_unit_circle = np.all(np.abs(model_results.maroots) > 1)
+    
+    if verbose:
+        print('Test heteroskedasticity of residuals ({}): stat={:.3f}, p={:.3f}'.format(het_method, het_stat, het_p));
+        print('\nTest normality of residuals ({}): stat={:.3f}, p={:.3f}'.format(norm_method, norm_stat, norm_p));
+        print('\nTest serial correlation of residuals ({}): stat={:.3f}, p={:.3f}'.format(sercor_method, sercor_stat, sercor_p));
+        print('\nDurbin-Watson test on residuals: d={:.2f}\n\t(NB: 2 means no serial correlation, 0=pos, 4=neg)'.format(dw_stat))
+        print('\nTest for all AR roots outside unit circle (>1): {}'.format(arroots_outside_unit_circle))
+        print('\nTest for all MA roots outside unit circle (>1): {}'.format(maroots_outside_unit_circle))
+    
+    stat = {'het_method': het_method,
+            'het_stat': het_stat,
+            'het_p': het_p,
+            'norm_method': norm_method,
+            'norm_stat': norm_stat,
+            'norm_p': norm_p,
+            'skew': skew,
+            'kurtosis': kurtosis,
+            'sercor_method': sercor_method,
+            'sercor_stat': sercor_stat,
+            'sercor_p': sercor_p,
+            'dw_stat': dw_stat,
+            'arroots_outside_unit_circle': arroots_outside_unit_circle,
+            'maroots_outside_unit_circle': maroots_outside_unit_circle,
+            }
+    return stat
+
+def model_gridsearch(ts,
+                     p_min,
+                     d_min,
+                     q_min,
+                     p_max,
+                     d_max,
+                     q_max,
+                     sP_min,
+                     sD_min,
+                     sQ_min,
+                     sP_max,
+                     sD_max,
+                     sQ_max,
+                     trends,
+                     s=None,
+                     enforce_stationarity=True,
+                     enforce_invertibility=True,
+                     simple_differencing=False,
+                     plot_diagnostics=False,
+                     verbose=False,
+                     filter_warnings=True,
+                    ):
+    '''Run grid search of SARIMAX models and save results.
+    '''
+    
+    cols = ['p', 'd', 'q', 'sP', 'sD', 'sQ', 's', 'trend',
+            'enforce_stationarity', 'enforce_invertibility', 'simple_differencing',
+            'aic', 'bic',
+            'het_p', 'norm_p', 'sercor_p', 'dw_stat',
+            'arroots_gt_1', 'maroots_gt_1',
+            'datetime_run']
+
+    # Initialize a DataFrame to store the results
+    df_results = pd.DataFrame(columns=cols)
+
+    # # Initialize a DataFrame to store the results
+    # results_bic = pd.DataFrame(index=['AR{}'.format(i) for i in range(p_min,p_max+1)],
+    #                            columns=['MA{}'.format(i) for i in range(q_min,q_max+1)])
+
+    mod_num=0
+    for trend,p,d,q,sP,sD,sQ in itertools.product(trends,
+                                                  range(p_min,p_max+1),
+                                                  range(d_min,d_max+1),
+                                                  range(q_min,q_max+1),
+                                                  range(sP_min,sP_max+1),
+                                                  range(sD_min,sD_max+1),
+                                                  range(sQ_min,sQ_max+1),
+                                                  ):
+        # initialize to store results for this parameter set
+        this_model = pd.DataFrame(index=[mod_num], columns=cols)
+
+        if p==0 and d==0 and q==0:
+            continue
+
+        try:
+            model = sm.tsa.SARIMAX(ts,
+                                   trend=trend,
+                                   order=(p, d, q),
+                                   seasonal_order=(sP, sD, sQ, s),
+                                   enforce_stationarity=enforce_stationarity,
+                                   enforce_invertibility=enforce_invertibility,
+                                   simple_differencing=simple_differencing,
+                                  )
+            
+            if filter_warnings is True:
+                with warnings.catch_warnings():
+                    warnings.filterwarnings("ignore")
+                    model_results = model.fit(disp=0)
+            else:
+                model_results = model.fit()
+
+            if verbose:
+                print(model_results.summary())
+
+            if plot_diagnostics:
+                model_results.plot_diagnostics();
+
+            stat = model_resid_stats(model_results,
+                                     verbose=verbose)
+
+            this_model.loc[mod_num, 'p'] = p
+            this_model.loc[mod_num, 'd'] = d
+            this_model.loc[mod_num, 'q'] = q
+            this_model.loc[mod_num, 'sP'] = sP
+            this_model.loc[mod_num, 'sD'] = sD
+            this_model.loc[mod_num, 'sQ'] = sQ
+            this_model.loc[mod_num, 's'] = s
+            this_model.loc[mod_num, 'trend'] = trend
+            this_model.loc[mod_num, 'enforce_stationarity'] = enforce_stationarity
+            this_model.loc[mod_num, 'enforce_invertibility'] = enforce_invertibility
+            this_model.loc[mod_num, 'simple_differencing'] = simple_differencing
+
+            this_model.loc[mod_num, 'aic'] = model_results.aic
+            this_model.loc[mod_num, 'bic'] = model_results.bic
+
+            # this_model.loc[mod_num, 'het_method'] = stat['het_method']
+            # this_model.loc[mod_num, 'het_stat'] = stat['het_stat']
+            this_model.loc[mod_num, 'het_p'] = stat['het_p']
+            # this_model.loc[mod_num, 'norm_method'] = stat['norm_method']
+            # this_model.loc[mod_num, 'norm_stat'] = stat['norm_stat']
+            this_model.loc[mod_num, 'norm_p'] = stat['norm_p']
+            # this_model.loc[mod_num, 'skew'] = stat['skew']
+            # this_model.loc[mod_num, 'kurtosis'] = stat['kurtosis']
+            # this_model.loc[mod_num, 'sercor_method'] = stat['sercor_method']
+            # this_model.loc[mod_num, 'sercor_stat'] = stat['sercor_stat']
+            this_model.loc[mod_num, 'sercor_p'] = stat['sercor_p']
+            this_model.loc[mod_num, 'dw_stat'] = stat['dw_stat']
+            this_model.loc[mod_num, 'arroots_gt_1'] = stat['arroots_outside_unit_circle']
+            this_model.loc[mod_num, 'maroots_gt_1'] = stat['maroots_outside_unit_circle']
+
+            this_model.loc[mod_num, 'datetime_run'] = pd.to_datetime('today').strftime('%Y-%m-%d %H:%M:%S')
+
+            df_results = df_results.append(this_model)
+            mod_num+=1
+        except:
+            continue
+    return df_results
+
+def tsdecompose(series, freq=None, plot=None):
+    decomposition = seasonal_decompose(series, freq=6)
+    if plot:
+        fig = plt.figure()
+        fig = decomposition.plot()
+        fig.set_size_inches(15, 8)
+        plt.show(block=False)
+    return decomposition
